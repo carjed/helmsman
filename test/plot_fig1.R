ebc20999b2959477b0711be01f749cac4ed57455
Jedidiah Carlson <jed.e.carlson@gmail.com>
2018-07-05 10:44:59 -0400

    update figures

--- test/plot_fig1.R
+++ test/plot_fig1.R
@@ -16,6 +16,7 @@ ggplot(performance_df, aes(x=program, y=vals, fill=program))+
   geom_col()+
   facet_wrap(~metric, scales="free", strip.position="left")+
   scale_y_continuous(expand=c(0,0))+
+  # coord_flip()+
   # scale_y_log10(expand=c(0,0), breaks=10^seq(0,4))+
   # scale_fill_manual(values=lacroix_palettes$Pamplemousse[1:4])+
   scale_fill_brewer(palette="Spectral", direction=-1)+
@@ -24,6 +25,11 @@ ggplot(performance_df, aes(x=program, y=vals, fill=program))+
   theme(legend.position="bottom",
         strip.background = element_blank(),
         strip.placement = "outside",
+        axis.text.y=element_text(size=14),
+        strip.text=element_text(size=16),
+        legend.title = element_blank(),
+        legend.text = element_text(size=14),
         axis.ticks.x=element_blank(),
         axis.text.x=element_blank(),
         axis.title.x=element_blank())
+ggsave("helmsman/assets/fig1_v3.png", width=12, height=6)
